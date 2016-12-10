3fa01edbb64ad4a65d7ea7cb3bdb857d7b28ee1b
x00115013 <first.hadron@gmail.com>
2016-12-10 00:11:59 +0000

    asp_Website_Azure
    Web application connection to azure database

---
+++ 4th_Year_BMS_Project/4th_Year_BMS_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4th_Year_BMS_Project.Startup))]
+namespace _4th_Year_BMS_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
